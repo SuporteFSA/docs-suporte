e83b804594ed4ab64049dc0f684b1bc2288c58d2
Leonardo de Araujo Evangelista <leonardo.evangelista@unifsa.fsa>
2025-06-17 16:53:41 -0300

    inserindo script div toggle elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,50 @@
 let btnItem = document.querySelectorAll('.dropdown-item')
 let divs = document.querySelectorAll('div')
 
-btnItem.forEach(btnItem => {
-    btnItem.addEventListener('click', (e)=>{
-    let btnId = btnItem.id
-    console.log(btnId)
+
+
+var getId = function getId(elementIdButton){
+    console.log(elementIdButton)
+    divs.forEach(divs => 
+        {
+            let div = divs
+        
+            if(div.hasAttribute('id'))
+            {
+                
+            let divContensId = div
+                if(divContensId.id !== elementIdButton)
+                {
+                    div.classList.add('oculta')
+                }else
+                {
+                    div.classList.remove('oculta')
+                    div.classList.add('visivel')
+                }
+            }
+        })
+}
+
+
+
+
+btnItem.forEach(btnItem => 
+    {
+        
+        btnItem.addEventListener('click', (e)=>
+        {
+                
+            btnId = btnItem.id
+              
+            getId(btnId)
+            
     
+        })
+
+ 
     })
-});
+
+
 
 /*divs.forEach(divs => {
    let divsId =  divs.id
